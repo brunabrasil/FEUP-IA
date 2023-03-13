419917201f1c926211e8ace71cb4a3fae9c18d79
brunabrasil <up202007191@up.pt>
2023-03-13 11:06:11 +0000

    added menu. work done by bruna, sofia and pedro

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import os
 import platform
 import time
 from board import Board
+import pygame_menu
+from pygame_menu import themes
+
 if platform.system() == 'Windows':
     os.environ['SDL_VIDEODRIVER'] = 'windib'
+
+
+ 
 
 
 board1 = np.array([
@@ -56,7 +62,46 @@ def main():
     pygame.init() 
     WINDOW_SIZE =(610,610)
     screen = pygame.display.set_mode(WINDOW_SIZE)
-    board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1],board2)
+    global board
+    board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1],board1)
+    def start_the_game():
+        submenu.disable()
+        mainmenu.disable()
+    
+    def level_menu():
+        mainmenu._open(submenu)
+    
+    def set_size(value, size):
+        print(value)
+        global board
+        if size == 1:
+            board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1],board1)
+        elif size == 2:
+            board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1],board2)
+        elif size == 3:
+            board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1],board3)
+
+        print(size)
+        
+    def set_mode(value, mode):
+        print(value)
+        print(mode)
+ 
+    mytheme = pygame_menu.themes.THEME_BLUE.copy()
+    mytheme.cursor_selection_color=(85, 203, 205)
+    mytheme.title_font_shadow = False
+    mytheme.title_font = pygame_menu.font.FONT_BEBAS
+    mainmenu = pygame_menu.Menu('WANA', 610, 610, theme=mytheme)
+    mainmenu.add.button('Play', level_menu)
+    mainmenu.add.button('Instructions', level_menu)
+    mainmenu.add.button('Quit', pygame_menu.events.EXIT)
+    
+    submenu = pygame_menu.Menu('Game Options', 610, 610, theme=mytheme)
+    submenu.add.selector('Mode: ', [('Player vs Player', 1), ('Player vs Computer', 2), ('Computer vs Computer', 3)], onchange=set_mode)
+    submenu.add.selector('Board Size:', [('Small', 1), ('Medium', 2), ('Large', 3)], onchange=set_size)
+    submenu.add.button('Start Game', start_the_game)
+    mainmenu.mainloop(screen)
+
     # load and set the logo
     #logo = pygame.image.load("logo.png")
     #pygame.display.set_icon(logo)
@@ -66,14 +111,16 @@ def main():
         board.draw(display)
         pygame.display.update()
 
-
+    arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (10, 15))
+ 
     # create a surface on screen that has the size of 240 x 180
     
     # define a variable to control the main loop
     running = True
     while running:
         mx, my = pygame.mouse.get_pos()
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             # Quit the game if the user presses the close button
             if event.type == pygame.QUIT:
                 running = False
@@ -81,10 +128,16 @@ def main():
                 # If the mouse is clicked
                 if event.button == 1:
                     if not board.handle_click(mx, my):
-                        
                         running=False
+            
+        if mainmenu.is_enabled():
+            mainmenu.update(events)
+            mainmenu.draw(screen)
+            if (mainmenu.get_current().get_selected_widget()):
+                arrow.draw(screen, mainmenu.get_current().get_selected_widget())
+ 
         draw(screen)
-    
+
     time.sleep(1)
      
      
